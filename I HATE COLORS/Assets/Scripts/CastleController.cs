75d99186c9b245de0aec332f9dbc5bd04dd4f6c2
cchu1008 <cchu.1008@gmail.com>
2018-01-25 21:29:12 -0800

    Added health to castle, button on menu screen takes you to main game, made script for whenever we need to switch scenes, added GameOver text, added temporary Health text for testing, moved components down so grid is centered.

---
+++ I HATE COLORS/Assets/Scripts/CastleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleController : MonoBehaviour {
+
+    private int health;
+	// Use this for initialization
+	void Start () {
+        health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameControl.instance.healthText.text = "Health: " + health.ToString();
+	}
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(health > 0)
+            health--;
+        if(health == 0)
+        {
+            GameControl.instance.gameOver = true;
+        }
+    }
+}
---
+++ I HATE COLORS/Assets/Scripts/GameControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+    public bool gameOver;
+    public Text gameOverText;
+    public Text healthText;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        gameOver = false;
+        gameOverText.text = "";
+        healthText.text = "Health: 100";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameOver)
+        {
+            healthText.text = "Health: 0";
+            gameOverText.text = "GAME OVER";
+        }
+	}
+}
