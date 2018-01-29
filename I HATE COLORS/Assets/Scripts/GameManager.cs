30494e9e1a017fa4ae71b3a5799e0e6470aa2481
David Askari <askari@mailbox.org>
2018-01-28 18:30:27 -0800

    Implement basic TD scene.

---
+++ I HATE COLORS/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField]
+    private int width = 15;
+
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+    }
+
+    [SerializeField]
+    private int heigth = 7;
+
+    public int Heigth
+    {
+        get
+        {
+            return heigth;
+        }
+    }
+
+    public TowerBtn SelectedTower { get; private set; }
+
+    public void SelectTower(TowerBtn towerBtn)
+    {
+        this.SelectedTower = towerBtn;
+        Hover.Instance.Activate(towerBtn.TowerHoverSprite);
+    }
+
+    private GameManager() { }
+
+    public void ResetTower()
+    {
+        this.SelectedTower = null;
+    }
+}
