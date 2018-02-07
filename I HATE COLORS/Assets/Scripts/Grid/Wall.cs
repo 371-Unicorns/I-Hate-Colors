da15d3888c1ffcd9e00cce345393a10ce8bfa9c2
cchu1008 <cchu.1008@gmail.com>
2018-02-07 10:59:10 -0800

    Unicorns 'splat' on impact. Can change audio clip later

--- I HATE COLORS/Assets/Scripts/Grid/Wall.cs
+++ I HATE COLORS/Assets/Scripts/Grid/Wall.cs
@@ -9,6 +9,15 @@ public class Wall : MonoBehaviour
 {
     [SerializeField]
     private GameObject explosion;
+
+    [SerializeField]
+    public AudioClip splat;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    private float volumeLow = 0.5f;
+    private float volumeHigh = 1.0f;
     /// <summary>
     /// Sent when another object enters a trigger collider attached to this object (2D physics only).
     /// Take damage, remove entered gameobject from list of active enemies and then destroy gameobject.
@@ -30,8 +39,13 @@ public class Wall : MonoBehaviour
     public void Explosion()
     {
         GameObject explosionClone = (GameObject)Instantiate(explosion, transform.position, transform.rotation);
-
+        AudioSource sourceClone = (AudioSource)Instantiate(audioSource, transform.position, transform.rotation);
         // You can also access other components / scripts of the clone
         Instantiate(explosionClone.GetComponent<ParticleSystem>(), transform.position, transform.rotation);
+        sourceClone.PlayOneShot(splat, Random.Range(volumeLow, volumeHigh));
+
+        Destroy(explosionClone.GetComponent<ParticleSystem>().gameObject);
+        Destroy(explosionClone.gameObject);
+        Destroy(sourceClone.gameObject);
     }
 }
