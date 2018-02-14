897914c1a1696e686219970486f3fa67df0dbe67
cchu1008 <cchu.1008@gmail.com>
2018-02-14 01:30:28 -0800

    Started setting up new laser tower. have a temp picture for the tower and an image for the laser. Started on the script. Not sure about effectiveness yet.

---
+++ I HATE COLORS/Assets/Scripts/Tower/Laser.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : Bullet {
+    
+    private Enemy target;
+    private Vector3 ourDir;
+
+    // Use this for initialization
+    void Start () {
+        ourDir = Vector3.left;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.transform.position - transform.position;
+        if (ourDir == Vector3.left)
+        {
+            ourDir = dir;
+            transform.LookAt(target.transform);
+        }
+        if((dir - ourDir).magnitude > 5)
+        {
+            Destroy(gameObject);
+            return;
+        }
+	}
+    
+    public new void Seek(Enemy _target)
+    {
+        target = _target;
+    }
+
+    public Vector3 GetDirection()
+    {
+        return ourDir;
+    }
+}
