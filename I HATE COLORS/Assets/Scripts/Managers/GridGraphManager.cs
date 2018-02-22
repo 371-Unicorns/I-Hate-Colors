d01eed3b022ba02954fd11978b1cf661a35432ef
Steven Johnson <sjsushiman@gmail.com>
2018-02-21 20:42:35 -0500

    Change position of UI elements

--- I HATE COLORS/Assets/Scripts/Managers/GridGraphManager.cs
+++ I HATE COLORS/Assets/Scripts/Managers/GridGraphManager.cs
@@ -35,6 +35,7 @@ public class GridGraphManager : Singleton<GridGraphManager>
         if (gridGraph == null) { gridGraph = AstarPath.active.data.gridGraph; }
 
         // Adjust in order to increase resolution of GridGraph
+        print(gridGraph);
         gridGraph.SetDimensions(width * 4, height * 4, tileSize / 4.0f);
 
         randomTargetTile = EnemyManager.Instance.GetRandomTargetTile();
