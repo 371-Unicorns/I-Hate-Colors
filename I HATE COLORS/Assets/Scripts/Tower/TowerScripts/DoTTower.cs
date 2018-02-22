730db687a450e8fc97a73c05531b72ba993afe8a
cchu1008 <cchu.1008@gmail.com>
2018-02-21 18:28:51 -0800

    Added Laser Tower to XML and added DoT section in the XML importer script.

--- I HATE COLORS/Assets/Scripts/Tower/TowerScripts/DoTTower.cs
+++ I HATE COLORS/Assets/Scripts/Tower/TowerScripts/DoTTower.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class DoTTower : Tower
 {
+    /// <summary>
+    /// Initialize this tower and activte attackTimer.
+    /// </summary>
+    /// <param name="name">Name of the tower.</param>
+    /// <param name="baseCosts">Base costs to build tower.</param>
+    /// <param name="upgradeCosts">Base costs to build tower.</param>
+    /// <param name="upgradeCostsScale">Scale of upgrade costs after each upgrade.</param>
+    /// <param name="maxLevel">Max level tower can reach.</param>
+    /// <param name="range">Range tower can attack within.</param>
+    /// <param name="effectDamage">Damage of the projectile.</param>
+    public void Initialize(string name, int baseCosts, int upgradeCosts, double upgradeCostsScale, int maxLevel, float range, float effectDamage)
+    {
+        base.Initialize(name, baseCosts, upgradeCosts, upgradeCostsScale, maxLevel, range);
+        effectPrefab.GetComponent<DoTEffect>().Initialize(effectDamage, range);
+    }
 
     public override void Update()
     {
