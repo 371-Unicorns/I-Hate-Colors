101aa1f6ec8dfc8461c16e57758639a22a182cfd
David Askari <askari@mailbox.org>
2018-03-03 18:49:08 -0800

    Cleanup PrefabHolder & fix Unicorn animation clipping.

--- I HATE COLORS/Assets/Scripts/UI/BloodFly.cs
+++ I HATE COLORS/Assets/Scripts/UI/BloodFly.cs
@@ -22,7 +22,7 @@ public class BloodFly : MonoBehaviour
     /// </summary>
     private void Start()
     {
-        target = GameManager.coinFlyTarget;
+        target = GameManager.bloodFlyTarget;
         bloodSound = GetComponent<AudioSource>();
     }
 
