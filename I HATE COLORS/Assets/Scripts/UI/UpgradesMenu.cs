b9bb6972f5997f13a97600330d67b994e11a4c1a
Twitch572 <dude.dodiecat.dudet@gmail.com>
2018-03-05 17:23:35 -0800

    Intro work on upgrades menu

---
+++ I HATE COLORS/Assets/Scripts/UI/UpgradesMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Menu for upgrades.
+/// </summary>
+public class UpgradesMenu : MonoBehaviour
+{
+
+    private int currentHealthLevel = 1;
+    public int upgradeHealthCost = 100;
+
+    public void UpgradeCastleHealth()
+    {
+        if (upgradeHealthCost <= GameManager.money)
+        {
+            GameManager.AddMoney(-upgradeHealthCost);
+
+            currentHealthLevel++;
+        }
+    }
+}
