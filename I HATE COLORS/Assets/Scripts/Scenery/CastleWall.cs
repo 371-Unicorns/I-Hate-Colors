2c153933b1e89b01d2c54f86a9d3f49a551365ad
Amy Lewis <alewis19@calpoly.edu>
2018-03-07 02:40:50 -0800

    Added post processing package, made it so the game loses color and you lose health - yes I know this is the opposite of what we want but we dont really have a win metric right now (how much money you need to win, how many waves to defeat, etc) so I put it there to demonstrate that I could get it to work.

--- I HATE COLORS/Assets/Scripts/Scenery/CastleWall.cs
+++ I HATE COLORS/Assets/Scripts/Scenery/CastleWall.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.PostProcessing;
 using UnityEngine;
 
 /// <summary>
@@ -16,6 +17,11 @@ public class CastleWall : MonoBehaviour
     public static Transform explosionParent;
 
     /// <summary>
+    /// Post Processing Profile.
+    /// </summary>
+    public PostProcessingProfile ppProfile;
+
+    /// <summary>
     /// AudioSource to be played, once a enemy reaches this castle wall.
     /// </summary>
     private AudioSource deathSound;
@@ -30,14 +36,46 @@ public class CastleWall : MonoBehaviour
     }
 
     /// <summary>
+    /// Enables Post Processing Behavior changes during run time.
+    /// </summary>
+    private void OnEnable()
+    {
+        var behaviour = GetComponent<PostProcessingBehaviour>();
+
+        if (behaviour.profile == null)
+        {
+            enabled = false;
+            return;
+        }
+
+        ppProfile = Instantiate(behaviour.profile);
+        behaviour.profile = ppProfile;
+    }
+
+    /// <summary>
     /// Take damage, remove entered gameobject from list of active enemies and then destroy gameobject.
     /// </summary>
     /// <param name="other">The other Collider2D involved in this collision.</param>
     private void OnTriggerEnter2D(Collider2D other)
     {
+        var saturation = ppProfile.colorGrading.settings;
+
         if (other.tag == "Enemy")
         {
             CastleManager.TakeDamage(1);
+
+            if (CastleManager.CastleHealth >= 100)
+            {
+                saturation.basic.saturation = 0;
+            }
+            else if (CastleManager.CastleHealth < 100 && CastleManager.CastleHealth > 0)
+            {
+                saturation.basic.saturation = ((float)CastleManager.CastleHealth / 100f);
+            }
+            else
+            {
+                saturation.basic.saturation = 1;
+            }
 
             ParticleSystem explosionParticle = Instantiate(explosionPrefab, other.transform.position, transform.rotation);
             explosionParticle.transform.SetParent(explosionParent);
@@ -48,5 +86,7 @@ public class CastleWall : MonoBehaviour
             Destroy(explosionParticle.gameObject, explosionParticle.main.duration);
             Destroy(other.gameObject);
         }
+
+        ppProfile.colorGrading.settings = saturation;
     }
 }
